064c369275baf7d2d4fea336f1abfef347896edc
Indigo <durovic.marek@gmail.com>
2021-02-15 18:21:18 +0100

    starting new project

---
+++ web/src/main/java/sk/durovic/issproject/IssProject/ServletInitializer.java
@@ -0,0 +1,13 @@
+package sk.durovic.issproject.IssProject;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(IssProjectApplication.class);
+	}
+
+}
---
+++ web/src/test/java/sk/durovic/issproject/IssProject/IssProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.durovic.issproject.IssProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
