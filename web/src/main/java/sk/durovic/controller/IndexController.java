2df929140fca22b548e4eb23a02b8172e8ec986c
Indigo <durovic.marek@gmail.com>
2021-02-22 21:15:05 +0100

    creating index page

--- web/src/main/java/sk/durovic/controller/IndexController.java
+++ web/src/main/java/sk/durovic/controller/IndexController.java
@@ -3,15 +3,25 @@ package sk.durovic.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import sk.durovic.model.Company;
+import sk.durovic.services.CompanyService;
+
+import java.util.Set;
 
 
 @Controller
 public class IndexController {
 
+    private final CompanyService companyService;
+
+    public IndexController(CompanyService companyService) {
+        this.companyService = companyService;
+    }
 
     @RequestMapping({"", "/", "/index"})
     public String getIndex(Model model){
-
+        Set<Company> listOfCompanies = companyService.findAll();
+        model.addAttribute("companies", listOfCompanies);
         return "index";
     }
 }
