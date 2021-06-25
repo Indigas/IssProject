7e5f086c938114bfa89889c1a6dc02d02a8c2cb4
Indigo <durovic.marek@gmail.com>
2021-06-25 23:17:01 +0200

    myerrorcontroller - changing code to implement errorviewresolver

--- web/src/main/java/sk/durovic/controller/MyErrorController.java
+++ web/src/main/java/sk/durovic/controller/MyErrorController.java
@@ -1,35 +1,17 @@
 package sk.durovic.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorViewResolver;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
 
 @Slf4j
 @Controller
-public class MyErrorController implements ErrorController {
-
-    @RequestMapping(value = "/error")
-    public ModelAndView getErrorPage(HttpServletResponse response){
-        ModelAndView modelAndView = new ModelAndView("/error/error");
-        Object statusCode = response.getStatus();
-        String errorMessage;
-
-        log.error("Error status:: " + statusCode);
-        errorMessage = getErrorMessage(Integer.parseInt(statusCode.toString()));
-
-        modelAndView.addObject("statusCode", statusCode.toString());
-        modelAndView.addObject("errorMessage", errorMessage);
-
-        return modelAndView;
-    }
+public class MyErrorController implements ErrorViewResolver {
 
     private String getErrorMessage(int errorMsg) {
         String msg;
@@ -41,14 +23,19 @@ public class MyErrorController implements ErrorController {
             case 500: msg= "Internal server error";
             break;
             default:
-                msg= "Some error occured";
+                msg= "Some error occurred";
         }
 
         return msg;
     }
 
     @Override
-    public String getErrorPath() {
-        return "/error";
+    public ModelAndView resolveErrorView(HttpServletRequest request, HttpStatus status, Map<String, Object> model) {
+        ModelAndView modelAndView = new ModelAndView("/error/error");
+
+        modelAndView.addObject("statusCode", status.value());
+        modelAndView.addObject("errorMessage", getErrorMessage(status.value()));
+
+        return modelAndView;
     }
 }
