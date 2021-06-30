399b703227a8508a08a79ae2d8588c977b7956a6
Indigo <durovic.marek@gmail.com>
2021-06-30 17:17:19 +0200

    testing company and car services

---
+++ data/src/test/java/sk/durovic/services/data/CarServiceImplTest.java
@@ -0,0 +1,117 @@
+package sk.durovic.services.data;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import sk.durovic.model.Car;
+import sk.durovic.model.Company;
+import sk.durovic.repositories.CarRepository;
+import sk.durovic.services.CarService;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import static org.hamcrest.MatcherAssert.*;
+
+
+@ExtendWith(MockitoExtension.class)
+class CarServiceImplTest {
+    @Mock
+    private CarRepository carRepository;
+
+    private Car car;
+    private CarService carService;
+
+    @BeforeEach
+    void setUp() {
+        carService = new CarServiceImpl(carRepository);
+
+        car = new Car();
+        car.setId(1L);
+        car.setBrand("audi");
+    }
+
+    @Test
+    void findAll() {
+        Set<Car> cars = Set.of(car, new Car());
+        Mockito.when(carRepository.findAll()).thenReturn(cars);
+
+        Set<Car> carList = carService.findAll();
+
+        Mockito.verify(carRepository, Mockito.atMostOnce()).findAll();
+        assertThat(carList, Matchers.hasSize(2));
+        assertThat(carList, Matchers.hasItem(car));
+    }
+
+    @Test
+    void findById() {
+        Mockito.when(carRepository.findById(1L)).thenReturn(Optional.ofNullable(car));
+
+        Car found = carService.findById(1L);
+
+        Mockito.verify(carRepository, Mockito.atMostOnce()).findById(1L);
+        assertThat(found, Matchers.notNullValue());
+        assertThat(found, Matchers.is(car));
+    }
+
+    @Test
+    void save() {
+        Mockito.when(carRepository.save(car)).thenReturn(car);
+
+        Car saved = carService.save(car);
+
+        Mockito.verify(carRepository, Mockito.atMostOnce()).save(car);
+        assertThat(saved, Matchers.notNullValue());
+        assertThat(saved, Matchers.is(car));
+    }
+
+    @Test
+    void delete() {
+        Mockito.doNothing().when(carRepository).delete(car);
+
+        carService.delete(car);
+
+        Mockito.verify(carRepository, Mockito.atMostOnce()).delete(car);
+    }
+
+    @Test
+    void deleteById() {
+        Mockito.doNothing().when(carRepository).deleteById(1L);
+
+        carService.deleteById(1L);
+
+        Mockito.verify(carRepository, Mockito.atMostOnce()).deleteById(1L);
+    }
+
+    @Test
+    void findByIsEnabled() {
+        Mockito.when(carRepository.findByIsEnabledTrue()).thenReturn(Optional.of(List.of(car, new Car())));
+
+        List<Car> list = carService.findByIsEnabled().orElse(null);
+
+        Mockito.verify(carRepository, Mockito.atMostOnce()).findByIsEnabledTrue();
+        assertThat(list, Matchers.notNullValue());
+        assertThat(list, Matchers.hasSize(2));
+        assertThat(list, Matchers.hasItem(car));
+
+    }
+
+    @Test
+    void findByCompany() {
+        Mockito.when(carRepository.findByCompanyId(1L)).thenReturn(Optional.of(List.of(car, new Car())));
+        Company company = new Company();
+        company.setId(1L);
+
+        List<Car> list = carService.findByCompany(company).orElse(null);
+
+        Mockito.verify(carRepository, Mockito.atMostOnce()).findByCompanyId(1L);
+        assertThat(list, Matchers.notNullValue());
+        assertThat(list, Matchers.hasSize(2));
+        assertThat(list, Matchers.hasItem(car));
+    }
+}
---
+++ data/src/test/java/sk/durovic/services/data/CompanyServiceImplTest.java
@@ -0,0 +1,133 @@
+package sk.durovic.services.data;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import sk.durovic.model.Company;
+import sk.durovic.repositories.CompanyRepository;
+import sk.durovic.services.CompanyService;
+
+import java.util.Optional;
+import java.util.Set;
+
+import static org.hamcrest.MatcherAssert.*;
+
+@ExtendWith(MockitoExtension.class)
+class CompanyServiceImplTest {
+
+    @Mock
+    private CompanyRepository companyRepository;
+
+    private CompanyService companyService;
+
+    private Company company;
+
+    @BeforeEach
+    void setUp() {
+        companyService = new CompanyServiceImpl(companyRepository);
+        company = new Company();
+        company.setId(1L);
+    }
+
+    @Test
+    void findAll() {
+        Set<Company> companies = Set.of(company, new Company());
+        Mockito.when(companyRepository.findAll()).thenReturn(companies);
+
+        Set<Company> companySet = companyService.findAll();
+
+        assertThat(companySet, Matchers.hasSize(2));
+        assertThat(companySet, Matchers.hasItem(company));
+    }
+
+
+    @Test
+    void save() {
+        Mockito.when(companyRepository.save(Mockito.any())).thenReturn(company);
+
+        Company saved = companyService.save(company);
+
+        Mockito.verify(companyRepository, Mockito.atMostOnce()).save(company);
+        assertThat(saved, Matchers.is(company));
+    }
+
+    @Test
+    void delete() {
+        Mockito.doNothing().when(companyRepository).delete(company);
+
+        companyService.delete(company);
+
+        Mockito.verify(companyRepository, Mockito.atMostOnce()).delete(company);
+    }
+
+    @Test
+    void deleteById() {
+        long id=1L;
+        Mockito.doNothing().when(companyRepository).deleteById(id);
+
+        companyService.deleteById(id);
+
+        Mockito.verify(companyRepository, Mockito.atMostOnce()).deleteById(id);
+
+    }
+
+    @Nested
+    class findByEmailMethods {
+        @Test
+        void findByEmail() {
+            String email = company.getEmail();
+            Mockito.when(companyRepository.findByEmail(email)).thenReturn(Optional.ofNullable(company));
+
+            Company found = companyService.findByEmail(email).orElse(null);
+
+            assertThat(found, Matchers.notNullValue());
+            assertThat(found, Matchers.is(company));
+        }
+
+        @Test
+        void notFindByEmail(){
+            String email = "nothing";
+            Mockito.when(companyRepository.findByEmail(email)).thenReturn(Optional.empty());
+
+            Company found = companyService.findByEmail(email).orElse(null);
+
+            assertThat(found, Matchers.nullValue());
+        }
+
+    }
+
+    @Nested
+    class FindByIdMethods {
+
+        @Test
+        void findById() {
+            long id = company.getId();
+            Mockito.when(companyRepository.findById(Mockito.any())).thenReturn(Optional.ofNullable(company));
+
+            Company found = companyService.findById(id);
+
+            Mockito.verify(companyRepository, Mockito.atMostOnce())
+                    .findById(id);
+
+            assertThat(found, Matchers.notNullValue());
+            assertThat(found, Matchers.samePropertyValuesAs(company));
+        }
+
+        @Test
+        void notFindById(){
+            long id = 5000L;
+            Mockito.when(companyRepository.findById(id)).thenReturn(Optional.empty());
+
+            Company found = companyService.findById(id);
+
+            Mockito.verify(companyRepository, Mockito.atMostOnce())
+                    .findById(5000L);
+            assertThat(found, Matchers.nullValue());
+        }
+    }
+}
